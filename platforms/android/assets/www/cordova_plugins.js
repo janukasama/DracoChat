9bcf781178c687c8321e0e30e755821cd3016b08
Januka Samaranayake <jpsamaranayake@virtusa.com>
2015-11-26 17:16:22 +0530

    Full Project

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-websocket/www/websocket.js",
+        "id": "cordova-plugin-websocket.websocket",
+        "pluginId": "cordova-plugin-websocket",
+        "clobbers": [
+            "WebSocket"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
